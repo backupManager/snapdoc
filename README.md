287c996d984f51da432effaa3102f807b8122644
Matt Rosendin <matt.rosendin@gmail.com>
2016-12-28 20:32:26 -0800

    continue tutorial, allow ATS exceptions, update readme

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Cabinet :open_file_folder:
 ==========================
 
 React Native OCR document cloud storage service
+
+## Quickstart
+
+Clone the repository and run `npm install`. The prerequisites in the [React Native Getting Started](http://facebook.github.io/react-native/releases/0.23/docs/getting-started.html#content) guide should be met for both iOS and Android. Then run the following commands to start the emulators:
+
+```
+$ react-native run-ios
+$ react-native run-android
+```
--- index.ios.js
+++ index.ios.js
@@ -7,13 +7,60 @@ import {
   View
 } from 'react-native';
 
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'https://i.imgur.com/UePbdph.jpg'}},
 ];
 
 export default class cabinet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+    };
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  };
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  };
+
   render() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
     return (
       <View style={styles.container}>
         <Image
